4021b9e10317d4be0249c96ded271040985375d2
murtadhoaaishah <aishatmurtadho@gmail.com>
2023-01-26 09:45:13 +0100

    first commitn

---
+++ src/About.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const About = () => {
+
+    const divStyle = {
+        color: 'white'
+    };
+
+
+
+    return (
+
+
+
+        <div style={divStyle}>
+            <h2>About Page</h2>
+            <main>
+                <p>This section contains information about...</p>
+            </main>
+        </div>
+
+
+    )
+}
+
+export default About
---
+++ src/App.js
@@ -0,0 +1,49 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <div>
+      <nav>
+        <ul id="navigation">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+      </Switch>
+    </div>n
+  );
+}
+
+export default App;
